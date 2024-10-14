5f2e5d333fbb560c2b7f3b74a4b5bd9666bac0bb
namraravani <namraravani8@gmail.com>
2024-10-14 18:40:41 +0530

    Add project files.

---
+++ SitecoreDemoWebsite/Areas/Blog/Controllers/PageController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitecoreDemoWebsite.Areas.Blog.Controllers
+{
+    public class PageController : Controller
+    {
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
